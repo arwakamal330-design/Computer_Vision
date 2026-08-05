026b189b7375b45c36878525436f07675933c779
arwakamal330-design <arwakamal330@gmail.com>
2026-08-05 06:40:17 +0300

    Update task_8_part_B.py

--- task_8_part_B.py
+++ task_8_part_B.py
@@ -1,9 +1,31 @@
 import cv2
 import numpy as np
 
-#def validate_mask(mask):
+# validate
+
+def validate_mask(mask):
+
+    print("validate_mask() executed")
+
+    white = cv2.countNonZero(mask)
+    total = mask.shape[0] * mask.shape[1]
+    coverage = (white / total) * 100
+
+    print("Coverage:", round(coverage, 1), "%")
+
+    contours, _ = cv2.findContours(mask,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
+
+    print("Separate regions:", len(contours))
+
+    if len(contours) == 1:
+        print("PASS")
+    else:
+        print("FAIL")
+
+    print("------------------")
 
 
+# Camera
 
 cap = cv2.VideoCapture(0)
 
@@ -29,16 +51,21 @@ while True:
 
     result = cv2.bitwise_and(frame, frame, mask=mask)
 
+    # Validate
+
+    validate_mask(mask)
+
     # Show
 
     cv2.imshow("Camera", frame)
     cv2.imshow("Mask", mask)
     cv2.imshow("Result", result)
 
-    key = cv2.waitKey(1) & 0xFF
+    # Exit
 
-    if key == ord('q'):
+    if cv2.waitKey(1) & 0xFF == ord("q"):
         break
 
 cap.release()
+
 cv2.destroyAllWindows()
